d680b2ebd96dc6254ea4354d12bb3d180e47a676
ethanhan2014 <laudy2012@gmail.com>
2015-10-16 00:17:43 -0400

    Collinear

---
+++ src/collinear/BruteCollinearPoints.java
@@ -0,0 +1,65 @@
+package collinear;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BruteCollinearPoints {
+	private int lineNum = 0 ;
+	private final List<LineSegment> lines;
+	/**
+	 * finds all line segments containing 4 points
+	 * @param points
+	 */
+	public BruteCollinearPoints(Point[] points) {
+		checkDuplicate(points);
+		lines = new ArrayList<LineSegment>();
+		int N = points.length;
+		Arrays.sort(points);
+		for(int i = 0; i<N-3; i++){
+			for(int j = i+1; j<N-2; j++){
+				for(int k = j+1; k<N-1; k++){
+					for(int l = k+1; l<N; l++){
+						if(points[i].slopeTo(points[j]) == points[j].slopeTo(points[k])){
+							if(points[j].slopeTo(points[k]) == points[k].slopeTo(points[l])){
+								LineSegment newline = new LineSegment(points[i],points[l]);
+								lines.add(newline);
+								lineNum += 1;
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * the number of line segments
+	 * @return the number of line segments
+	 */
+	public int numberOfSegments(){
+		return lineNum;
+	}
+	
+	/**
+	 * the line segments
+	 * @return the array of line segments
+	 */
+	public LineSegment[] segments(){
+		LineSegment[] segments = new LineSegment[lines.size()];
+		segments = lines.toArray(segments);
+		return segments;
+	}
+	
+	private void checkDuplicate(Point[] points){
+		Set<Point> setPoint = new HashSet<Point>();
+		  for (Point i : points)
+		  {
+		    if (setPoint.contains(i)) throw new IllegalArgumentException();
+		    setPoint.add(i);
+		  }
+	}
+
+}
---
+++ src/collinear/FastCollinearPoints.java
@@ -0,0 +1,77 @@
+package collinear;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FastCollinearPoints {
+	private int num = 0;
+	private final List<LineSegment> lines;
+
+	public FastCollinearPoints(Point[] points) {
+		checkDuplicate(points);
+		lines = new ArrayList<LineSegment>();
+		int N = points.length;
+		//sort points by coordinates
+		Arrays.sort(points);
+		Point[] aux = new Point[N];
+
+		for(int i = 0; i< N-3; i++){
+
+			for (int j =0; j< N; j++){
+				aux[j] = points[j];
+			}
+
+			//sort points before and after point[i] by slope
+			Arrays.sort(aux,0,i,aux[i].slopeOrder());
+			Arrays.sort(aux,i+1,N,aux[i].slopeOrder());
+
+			int head = i+1;
+			int tail = i+2;
+			int pHead = 0;
+
+			while(tail<N){
+				//if slope equal, move tail forward
+				double headSlope = aux[i].slopeTo(aux[head]);
+				while(tail<N && headSlope == aux[i].slopeTo(aux[tail])) tail++;
+				//slopes not equal, check if it is a segment
+				if(tail - head >=3){
+					double pSlope = Double.NEGATIVE_INFINITY;
+					while(pHead<i){
+						pSlope = aux[i].slopeTo(aux[pHead]);
+						if(pSlope < headSlope) pHead++;
+						else	break;
+					}
+					if(pSlope != headSlope){
+						LineSegment newline = new LineSegment(aux[i],aux[tail-1]);
+						lines.add(newline);
+						num++;
+					}
+				}
+				head = tail;
+				tail++;
+			}		
+		}
+	}
+
+	public int numberOfSegments(){
+		return num;
+	}
+
+	public LineSegment[] segments(){
+		LineSegment[] segments = new LineSegment[lines.size()];
+		segments = lines.toArray(segments);
+		return segments;
+	}
+	
+	private void checkDuplicate(Point[] points){
+		Set<Point> setPoint = new HashSet<Point>();
+		  for (Point i : points)
+		  {
+		    if (setPoint.contains(i)) throw new IllegalArgumentException();
+		    setPoint.add(i);
+		  }
+	}
+}
